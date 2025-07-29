1b675b69fabc2e8f9dbc3c8c01953abd3a3521ea
BotAutoTrading <adildisel@mail.ru>
2025-07-29 20:41:19 +0300

    add new service telegabotat with spring boot psql

---
+++ TelegaBotAT/telegabotat/src/main/java/org/example/telegabotat/TelegabotatApplication.java
@@ -0,0 +1,13 @@
+package org.example.telegabotat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegabotatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegabotatApplication.class, args);
+	}
+
+}
---
+++ TelegaBotAT/telegabotat/src/test/java/org/example/telegabotat/TelegabotatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.telegabotat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegabotatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
