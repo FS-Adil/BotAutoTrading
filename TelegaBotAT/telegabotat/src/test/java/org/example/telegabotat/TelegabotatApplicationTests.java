9e285169f609b70b467c9a7638b16f909806f262
BotAutoTrading <adildisel@mail.ru>
2025-08-08 21:47:00 +0300

    change release/01.003.00/ change pom.xml

--- TelegaBotAT/telegabotat/src/test/java/org/example/telegabotat/TelegabotatApplicationTests.java
+++ TelegaBotAT/telegabotat/src/test/java/org/example/telegabotat/TelegabotatApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TelegabotatApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
